c326906f7a9a018b29e333db1e65195b68feb69b
Flynn <wphestiraid@gmail.com>
2020-02-11 22:27:11 +0900

    디펜던시 정리 및 import 상대경로로 수정

--- src/controllers/user/logIn.ts
+++ src/controllers/user/logIn.ts
@@ -1,38 +1,36 @@
-import { Request, Response } from "express";
-import User from "../../entities/user";
-import { createToken } from "src/utils/auth";
+import { Request, Response } from 'express';
+import User from '../../entities/user';
+import { createToken } from '../../utils/auth';
 
 interface LogInRequest extends Request {
   body: {
     email: string;
     password: string;
-  }
+  };
 }
 
 const logIn = async (req: LogInRequest, res: Response): Promise<Response> => {
   const {
-    body: {
-      email, password
-    }
+    body: { email, password }
   } = req;
-  const user = await User.findOne({ email })
+  const user = await User.findOne({ email });
   if (!user) {
     // TODO: 커스텀 에러로 대체 예정
-    throw Error('존재하지 않는 이메일입니다.')
+    throw Error('존재하지 않는 이메일입니다.');
   }
-  const checkPassword = await user.comparePassword(password)
+  const checkPassword = await user.comparePassword(password);
   if (!checkPassword) {
     // TODO: 커스텀 에러로 대체 예정
-    throw Error('입력하신 정보가 올바르지 않습니다.')
+    throw Error('입력하신 정보가 올바르지 않습니다.');
   }
-  const token = createToken(user)
+  const token = createToken(user);
   return res.status(200).json({
     success: true,
     message: null,
     result: {
       token
     }
-  })
-}
+  });
+};
 
-export default logIn
+export default logIn;
