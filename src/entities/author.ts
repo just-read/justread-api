960da6d72879aed7867ee6b44a0d21f1c673cb7d
Flynn <wphestiraid@gmail.com>
2020-02-13 22:43:21 +0900

    기본 Author 모델 생성

---
+++ src/entities/author.ts
@@ -0,0 +1,20 @@
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+export enum AuthorType {
+  WRITER = 'writer',
+  TRANSLATOR = 'translator'
+}
+
+@Entity()
+class Author extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'enum', enum: AuthorType, default: AuthorType.WRITER })
+  type!: AuthorType;
+
+  @Column({ type: 'varchar', length: 30 })
+  name!: string;
+}
+
+export default Author;
