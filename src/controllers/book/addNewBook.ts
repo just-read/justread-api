8b755c081b152940b630ea5fc4d072bf5cf8a442
Flynn <wphestiraid@gmail.com>
2020-02-19 09:43:39 +0900

    새 책 추가 API 추가

---
+++ src/controllers/book/addNewBook.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+import Book from '../../entities/book';
+import { InvalidParamError } from '../../utils/customErrors';
+
+interface AddNewBookRequest extends Request {
+  body: {
+    title: string | undefined;
+    description: string | undefined;
+    year: number | undefined;
+  };
+}
+
+const addNewBook = async (req: AddNewBookRequest, res: Response): Promise<Response> => {
+  const {
+    body: { title, description, year }
+  } = req;
+
+  if (!title || !description || !year) {
+    throw new InvalidParamError('필요한 정보가 누락되었습니다.');
+  }
+
+  const newBook = await Book.create({ title, description, year }).save();
+  return res.status(201).json({
+    success: true,
+    message: null,
+    result: {
+      bookInfo: newBook
+    }
+  });
+};
+
+export default addNewBook;
--- src/routes/v1/book.ts
+++ src/routes/v1/book.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import getBookList from '../../controllers/book/getBookList';
 import getBookDetails from '../../controllers/book/getBookDetails';
+import addNewBook from '../../controllers/book/addNewBook';
 
 const bookRouter = Router();
 
 bookRouter.get('', getBookList);
 bookRouter.get('/:bookUniqueId', getBookDetails);
+bookRouter.post('', addNewBook);
 
 export default bookRouter;
