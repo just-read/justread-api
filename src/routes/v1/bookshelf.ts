51aeb1a39e0f57827871aa29fd7e1e109897e7df
Flynn <wphestiraid@gmail.com>
2020-02-22 21:35:24 +0900

    책장 상세 정보 API 라우터에 추가

--- src/routes/v1/bookshelf.ts
+++ src/routes/v1/bookshelf.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { privateRoute } from '../../utils/auth';
 import { getBookshelfList, createNewBookshelf } from '../../controllers/bookshelfControllers';
+import getBookshelfDetails from '../../controllers/bookshelf/getBookshelfDetailss';
 
 const bookshelfRouter = Router();
 
 bookshelfRouter.get('', privateRoute, getBookshelfList);
+bookshelfRouter.get('/:bookshelfId', privateRoute, getBookshelfDetails);
 bookshelfRouter.post('', privateRoute, createNewBookshelf);
 
 export default bookshelfRouter;
