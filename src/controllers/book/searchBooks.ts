904cdf6ab533838f4b2bd4174470a051664504a9
Flynn <wphestiraid@gmail.com>
2020-02-23 19:46:07 +0900

    책 검색 API 구현

--- src/controllers/book/searchBooks.ts
+++ src/controllers/book/searchBooks.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import { getRepository, Like } from 'typeorm';
+import Book from '../../entities/book';
 
 interface SearchBooksRequest extends Request {
   query: {
@@ -12,10 +14,41 @@ const searchBooks = async (
   next: NextFunction
 ): Promise<void> => {
   const {
-    query: { q: searchQuery }
+    query: { q: searchTerm }
   } = req;
-  if (searchQuery) {
+  if (searchTerm) {
     try {
+      const isISBN = (term: string): boolean => {
+        const isbnRegexp = new RegExp(/^.d{13}$/);
+        if (isbnRegexp.test(term)) {
+          return true;
+        }
+        return false;
+      };
+
+      let books: [Book[], number] = [[], 0];
+      if (isISBN(searchTerm)) {
+        books = await getRepository(Book).findAndCount({
+          where: {
+            isbn: searchTerm
+          }
+        });
+      } else {
+        books = await getRepository(Book)
+          .createQueryBuilder('book')
+          .where(`book.title LIKE '%${searchTerm}%'`)
+          .orWhere(`book.author LIKE '%${searchTerm}%`)
+          .getManyAndCount();
+      }
+
+      res.status(200).json({
+        success: true,
+        message: null,
+        result: {
+          books: books[0],
+          count: books[1]
+        }
+      });
     } catch (error) {
       next(error);
     }
