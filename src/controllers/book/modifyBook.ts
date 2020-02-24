375dd7cc2163cf210c07864b805ac861aeeebf5a
Flynn <wphestiraid@gmail.com>
2020-02-24 09:39:40 +0900

    책 수정 API 책이 존재하지 않을 때 처리 추가

--- src/controllers/book/modifyBook.ts
+++ src/controllers/book/modifyBook.ts
@@ -1,8 +1,8 @@
 import { Response, NextFunction } from 'express';
-import { CustomRequest } from '../../utils/auth';
-import { UnauthorizedError, InvalidParamError } from '../../utils/customErrors';
 import { getRepository } from 'typeorm';
 import Book from '../../entities/book';
+import { CustomRequest } from '../../utils/auth';
+import { UnauthorizedError, InvalidParamError, NotFoundError } from '../../utils/customErrors';
 
 interface ModifyBookRequest extends CustomRequest {
   body: {
@@ -31,6 +31,12 @@ const modifyBook = async (
 
     if (!uniqueBookId || !title || !isbn || !authors) {
       throw new InvalidParamError('필요한 정보가 누락되었습니다.');
+    }
+
+    const existingBook = await getRepository(Book).findOne({ uniqueId: uniqueBookId });
+
+    if (!existingBook) {
+      throw new NotFoundError('책 정보가 존재하지 않습니다.');
     }
 
     const modifiedBook = await getRepository(Book).update(
