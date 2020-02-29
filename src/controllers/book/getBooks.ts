e405a3792d6a2ce57ae8aa69a500e5b9f52dd5cb
Flynn <wphestiraid@gmail.com>
2020-02-29 20:56:23 +0900

    책 목록 API 수정
    - type 쿼리 파라미터가 정상 작동하도록 수정
    - 응답 오브젝트 수정

--- src/controllers/book/getBooks.ts
+++ src/controllers/book/getBooks.ts
@@ -3,9 +3,9 @@ import Book from '../../entities/book';
 import { InvalidParamError } from '../../utils/customErrors';
 
 export enum EnumBookListType {
-  RECENT = 'recent',
-  POPULAR = 'popular',
-  RECOMMEND = 'recomend'
+  recent = 'recent',
+  popular = 'popular',
+  recommend = 'recomend'
 }
 
 interface GetBookListRequest extends Request {
@@ -16,6 +16,12 @@ interface GetBookListRequest extends Request {
   };
 }
 
+interface InterfaceBookList {
+  bookItems: Book[];
+  total: number;
+  count: number;
+}
+
 const getBooks = async (
   req: GetBookListRequest,
   res: Response,
@@ -23,30 +29,56 @@ const getBooks = async (
 ): Promise<void> => {
   try {
     const {
-      query: { type = EnumBookListType.RECENT, page = 1, limit = 10 }
+      query: { type = EnumBookListType.recent, page = 1, limit = 10 }
     } = req;
-
     if (!(type in EnumBookListType)) {
       throw new InvalidParamError('type이 올바르지 않습니다.');
     }
 
     const offset = page * limit;
-    const getRecentBookList = async (): Promise<[Book[], number]> =>
-      Book.findAndCount({
+
+    const getRecentBookListInfo = async (): Promise<InterfaceBookList> => {
+      const recentTotal = await Book.count();
+      const recentInfo = await Book.findAndCount({
         take: limit,
         skip: offset,
         order: {
           id: 'DESC'
         }
       });
+      return {
+        bookItems: recentInfo[0],
+        count: recentInfo[1],
+        total: recentTotal
+      };
+    };
 
-    const bookItems = await getRecentBookList();
+    let initialBookItemsInfo: InterfaceBookList = {
+      bookItems: [],
+      total: 0,
+      count: 0
+    };
+
+    // 모양이 영 마음에 안 드는데 더 깔끔한 방법이 생기면 수정하기로 하자
+    switch (type) {
+      default:
+        const recentInfo = await getRecentBookListInfo();
+        initialBookItemsInfo = {
+          ...recentInfo
+        };
+    }
 
     res.status(200).json({
       success: true,
       message: null,
       result: {
-        bookItems
+        bookItems: initialBookItemsInfo.bookItems,
+        pageInfo: {
+          total: initialBookItemsInfo.total,
+          current: page,
+          limit,
+          count: initialBookItemsInfo.count
+        }
       }
     });
   } catch (error) {
