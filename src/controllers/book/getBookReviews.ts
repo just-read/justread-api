e1f6f5a23de990690f66a18eb1b3925d1a7365fc
Flynn <wphestiraid@gmail.com>
2020-03-03 22:23:42 +0900

    책의 리뷰들 API 구현

--- src/controllers/book/getBookReviews.ts
+++ src/controllers/book/getBookReviews.ts
@@ -1,7 +1,14 @@
-import { Response, NextFunction } from 'express';
-import { CustomRequest } from '../../utils/auth';
+import { Request, Response, NextFunction } from 'express';
+import { getRepository } from 'typeorm';
+import Book from '../../entities/book';
+import Rating from '../../entities/rating';
+import { NotFoundError } from '../../utils/customErrors';
 
-interface GetBookReviewsRequest extends CustomRequest {}
+interface GetBookReviewsRequest extends Request {
+  params: {
+    bookUniqueId: string;
+  };
+}
 
 // 특정 책에 대한 다른 사람들의 별점과 평가들을 가져옴
 const getBookReviews = async (
@@ -10,10 +17,31 @@ const getBookReviews = async (
   next: NextFunction
 ): Promise<void> => {
   try {
+    const {
+      params: { bookUniqueId }
+    } = req;
+
+    const book = await getRepository(Book).findOne({ uniqueId: bookUniqueId });
+
+    if (!book) {
+      throw new NotFoundError('일치하는 정보를 찾을 수 없습니다.');
+    }
+
+    const [reviews, count] = await getRepository(Rating)
+      .createQueryBuilder('rating')
+      .innerJoinAndSelect('rating.review', 'review')
+      .where('ratind.bookId = :bookId', { bookId: book.id })
+      .limit(10)
+      .orderBy('rating.id', 'DESC')
+      .getManyAndCount();
+
     res.status(200).json({
       success: true,
       message: null,
-      result: {}
+      result: {
+        reviews,
+        count
+      }
     });
   } catch (error) {
     next(error);
