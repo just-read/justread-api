2f00c8ffcecda7665ff8f729cfa87f00b8c8f5eb
Flynn <wphestiraid@gmail.com>
2020-03-03 22:17:26 +0900

    책의 내 리뷰 API 구현

--- src/controllers/book/getBookReview.ts
+++ src/controllers/book/getBookReview.ts
@@ -1,8 +1,15 @@
 import { Response, NextFunction } from 'express';
+import { getRepository } from 'typeorm';
+import Book from '../../entities/book';
+import Rating from '../../entities/rating';
 import { CustomRequest } from '../../utils/auth';
-import { UnauthorizedError } from '../../utils/customErrors';
+import { UnauthorizedError, NotFoundError } from '../../utils/customErrors';
 
-interface GetBookReviewRequest extends CustomRequest {}
+interface GetBookReviewRequest extends CustomRequest {
+  params: {
+    bookUniqueId: string;
+  };
+}
 
 // 특정 책에 대한 자신의 별점과 평가를 가져옴
 const getBookReview = async (
@@ -15,10 +22,29 @@ const getBookReview = async (
       throw new UnauthorizedError('인증 정보가 존재하지 않습니다.');
     }
 
+    const {
+      params: { bookUniqueId }
+    } = req;
+
+    const book = await getRepository(Book).findOne({ uniqueId: bookUniqueId });
+
+    if (!book) {
+      throw new NotFoundError('일치하는 정보를 찾을 수 없습니다.');
+    }
+
+    const bookReview = await getRepository(Rating)
+      .createQueryBuilder('rating')
+      .leftJoinAndSelect('rating.review', 'review')
+      .where('rating.id = :bookId', { bookId: book.id })
+      .andWhere('rating.userId = :userId', { userId: req.user.id })
+      .getOne();
+
     res.status(200).json({
       success: true,
       message: null,
-      result: {}
+      result: {
+        review: bookReview || null // 결과를 이렇게 줘도 되나...
+      }
     });
   } catch (error) {
     next(error);
