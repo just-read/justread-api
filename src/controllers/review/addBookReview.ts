e1e768f4ecad3fc8ec570caf930292cfd3bafb69
Flynn <wphestiraid@gmail.com>
2020-03-04 21:45:06 +0900

    책 리뷰 작성시 별점이 선행되지 않으면 에러 출력

--- src/controllers/review/addBookReview.ts
+++ src/controllers/review/addBookReview.ts
@@ -37,34 +37,21 @@ const addBookReview = async (
     /**
      * 해당 책의 별점이 존재하는지 먼저 확인하고
      * 별점이 있으면 별점의 ratingId를 가지는 리뷰 생성
-     * 별점이 없으면 0점짜리 별점을 생성하고 해당 별점의 ratingId를 가지는 리뷰 생성
+     * 별점이 없으면 에러 반환
      */
 
     const rating = await getRepository(Rating).findOne({ bookId: book.id, userId });
-    let newReview: Review;
 
-    if (rating) {
-      // 별점이 존재하므로 별점의 ratingId를 가지는 리뷰 생성
-      newReview = await Review.create({
-        userId,
-        bookId: book.id,
-        ratingId: rating.id,
-        content: reviewContent
-      }).save();
-    } else {
-      // 별점이 존재하지 않으므로 0점짜리 별점을 생성
-      const newRating = await Rating.create({
-        userId,
-        bookId: book.id,
-        rating: 0
-      }).save();
-      newReview = await Review.create({
-        userId,
-        bookId: book.id,
-        ratingId: newRating.id,
-        content: reviewContent
-      }).save();
+    if (!rating) {
+      throw new NotFoundError('해당 책의 별점을 찾을 수 없습니다.');
     }
+    // 별점이 존재하므로 별점의 ratingId를 가지는 리뷰 생성
+    const newReview = await Review.create({
+      userId,
+      bookId: book.id,
+      ratingId: rating.id,
+      content: reviewContent
+    }).save();
 
     res.status(201).json({
       success: true,
